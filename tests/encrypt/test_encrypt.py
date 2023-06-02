c741e686f4bf032b3bb13420cb6d4ead87038c9d
Andrew Lima <andrewmll94@gmail.com>
2023-06-02 16:16:22 -0300

    Feat: Conclusão do requisito 2(Eu acho) :rocket:

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(123, 'key')
+
+    assert encrypt_message('undefined', 10) == 'denifednu'
+    assert encrypt_message('typescript', 4) == 'tpircs_epyt'
+    assert encrypt_message('react', 1) == 'r_tcae'
