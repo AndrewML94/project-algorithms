2253109660d5a8dafcbebd6d04f2986b3e06fa2f
Andrew Lima <andrewmll94@gmail.com>
2023-06-02 17:05:48 -0300

    Feat: Conclusão do requisito 3 :rocket:

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    low_i = low_index
+    high_i = high_index
+    if word == '' or word[low_i] != word[high_i]:
+        return False
+    elif low_index >= high_index:
+        return True
+    else:
+        low_i += 1
+        high_i -= 1
+        return is_palindrome_recursive(word, low_i, high_i)
