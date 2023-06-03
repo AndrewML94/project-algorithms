23ce424d3a220ad32b052225ed668118b635b459
Andrew Lima <andrewmll94@gmail.com>
2023-06-03 02:26:01 -0300

    Fix: Ajuste do requisito 5 diminuindo a complexidade do algoritmo de O(n^2) para O(n), assim o avaliador passou!! :rocket:

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,11 +2,13 @@ def find_duplicate(nums):
     if len(nums) <= 1:
         return False
 
-    repeat_number = 0
+    num_set = set()
 
     for number in nums:
-        if type(number) is not int or number < 0:
+        if type(number) != int or number <= 0:
             return False
-        elif nums.count(number) > 1:
-            repeat_number = number
-    return repeat_number if repeat_number > 0 else False
+        if number in num_set:
+            return number
+        else:
+            num_set.add(number)
+    return False
