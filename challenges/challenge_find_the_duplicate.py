80d3ce36796c55275851b0f3a5bd797f58ab9228
Andrew Lima <andrewmll94@gmail.com>
2023-06-03 01:19:40 -0300

    Feat: Teste do req 5, computador pediu arrego ao rodar teste:rocket:

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+
+    repeat_number = 0
+
+    for number in nums:
+        if type(number) is not int or number < 0:
+            return False
+        elif nums.count(number) > 1:
+            repeat_number = number
+    return repeat_number if repeat_number > 0 else False
